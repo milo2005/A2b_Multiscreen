4c70f0c3f518ab311992f835d4b32e4a457b418a
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-08-10 20:46:52 -0500

    Creacion del proyecto, interfaces de MainActivity, MasterFragment y DetailFragment implementadas

---
+++ app/src/main/java/creatic/movil/multiscreen/fragments/DetailFragment.java
@@ -0,0 +1,31 @@
+package creatic.movil.multiscreen.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import creatic.movil.multiscreen.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailFragment extends Fragment {
+
+
+    public DetailFragment() {
+        
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+
+}
